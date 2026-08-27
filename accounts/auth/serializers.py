63c6eb40b74918938c3945d0dbed2548c87ab8b7
Calvin Kamp <calvin.kamp6696@gmail.com>
2026-08-27 16:26:33 +0200

    feat(accounts): add registration view and auth-user-model

---
+++ accounts/auth/serializers.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+
+from accounts.models import User
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    repeated_password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+
+        fields = ("id", "username", "email", "password", "repeated_password", "type")
+
+        extra_kwargs = {
+            "password": {
+                "write_only": True,
+            },
+            "type": {
+                "required": True,
+            },
+        }
+
+    def validate(self, attrs):
+        if attrs["password"] != attrs["repeated_password"]:
+            raise serializers.ValidationError(
+                {"repeated_password": "Passwords do not match."}
+            )
+
+        return super().validate(attrs)
+
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+        validated_data.pop("repeated_password")
+
+        user = User.objects.create(**validated_data)
+        user.set_password(password)
+        user.save()
+
+        return user
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    class RoleChoices(models.TextChoices):
+        BUSINESS = "business"
+        CUSTOMER = "customer"
+
+    type = models.TextField(choices=RoleChoices, max_length=8)
+    email = models.EmailField(unique=True, blank=False)
+
+    REQUIRED_FIELDS = ["email", "type"]
