e4f75d387f9c68e7ef8040a87991f9ee7f47fba2
Calvin Kamp <calvin.kamp6696@gmail.com>
2026-08-31 12:36:37 +0200

    feat(accounts): add profile-model

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone
 
 
 class User(AbstractUser):
@@ -11,3 +13,23 @@ class User(AbstractUser):
     email = models.EmailField(unique=True, blank=False)
 
     REQUIRED_FIELDS = ["email", "type"]
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        related_name="user",
+        on_delete=models.CASCADE,
+    )
+
+    first_name = models.CharField(blank=True, default="")
+    last_name = models.CharField(blank=True, default="")
+    location = models.CharField(blank=True, default="")
+    tel = models.CharField(blank=True, default="")
+    description = models.CharField(blank=True, default="")
+    working_hours = models.CharField(blank=True, default="")
+
+    type = models.TextField()
+    email = models.EmailField()
+
+    created_at = models.DateTimeField(auto_now_add=timezone.now)
