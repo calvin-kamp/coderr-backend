c29f5759e3ef8755b0046784754970c7308f5633
Calvin Kamp <calvin.kamp6696@gmail.com>
2026-08-31 12:37:16 +0200

    feat(accounts): add profile-views and routes

--- core/urls.py
+++ core/urls.py
@@ -21,4 +21,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("accounts.auth.urls")),
+    path("api/", include("accounts.profile.urls")),
 ]
